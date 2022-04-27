a509076c01b990e09d283e9e59b3ab0a4ff1c259
Nirvik Basnet <46699486+nirvikBasnet@users.noreply.github.com>
2022-04-27 19:53:58 +1000

    chore: more refactoring

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,56 +1,46 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const jwt = require('jsonwebtoken')
-const dotenv = require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv").config();
 const jwtkey = process.env.JWT_KEY;
 
-
 const router = express.Router();
-const User = mongoose.model('User');
+const User = mongoose.model("User");
 
-router.post('/signup',async (req,res)=>{
-    //console.log(req.body)
-    const {email,password} = req.body;
-    try{
+router.post("/signup", async (req, res) => {
+  //console.log(req.body)
+  const { email, password } = req.body;
+  try {
+    const user = new User({ email, password });
 
-        const user = new User({email,password});
+    await user.save();
 
-        await user.save();
-
-        const token = jwt.sign({userId:user._id},jwtkey) //id from mongodb
-        res.send({token})      
-    }catch(err){
-       return res.status(422).send(err.message)
-         
-    }
-
-    
-})
-
+    const token = jwt.sign({ userId: user._id }, jwtkey); //id from mongodb
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send(err.message);
+  }
+});
 
 //signin route
 
-router.post('/signin',async(req,res)=>{
-    const {email,password} = req.body;
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
 
-    if(!email || !password){
-        return res.status(422).send({error : "Must provide email or password."})
-    }
-    const user =await User.findOne({email}) //it might take some time to find email.
-    if(!user) {
-        return res.status(422).send({error : "User not Found !!"})
-    }
-    try{
+  if (!email || !password) {
+    return res.status(422).send({ error: "Must provide email or password." });
+  }
+  const user = await User.findOne({ email }); //it might take some time to find email.
+  if (!user) {
+    return res.status(422).send({ error: "User not Found !!" });
+  }
+  try {
     await user.comparePassword(password);
-    const token = jwt.sign({userId:user._id},jwtkey) //id from mongodb
-    res.send({token})
-    
-    }catch{
-        return res.status(422).send({error : "User not Found !!"})
+    const token = jwt.sign({ userId: user._id }, jwtkey); //id from mongodb
+    res.send({ Message: "Login Successful" });
+  } catch {
+    return res.status(422).send({ error: "User not Found !!" });
+  }
+});
 
-    }
-
-})
-
-
-module.exports = router
+module.exports = router;
